eb036ac05e0a5b04589444a8d39fcbd4daf3056e
miumio <nickolskaya777@yandex.ru>
2020-08-11 20:04:57 +0300

    😱

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require('gulp-csso');
+const rename = require("gulp-rename");
+const svgstore = require("gulp-svgstore");
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const del = require("del");
 
 // Styles
 
@@ -16,12 +22,46 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+//Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+      ]))
+}
+
+//Webp
+
+const webpp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.webpp = webpp;
+
+//Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
 
 // Server
 
@@ -49,3 +89,35 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+//Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+//Del
+
+const clean = () => {
+  return del("build");
+};
+
+//Build
+
+const build = () => gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+  );
